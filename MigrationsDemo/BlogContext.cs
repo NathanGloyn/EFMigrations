9562d6a45e601001194707aeb5c748eb50fd0442
NathanGloyn <nathans.dropbox@gmail.com>
2013-05-31 19:59:05 +0100

    Add post class

--- MigrationsDemo/BlogContext.cs
+++ MigrationsDemo/BlogContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace MigrationsDemo
 {
@@ -12,5 +14,18 @@ namespace MigrationsDemo
         public int BlogId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public int Rating { get; set; }
+        public List<Post> Posts { get; set; }
     }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }  
 }
